956205daf0bccb9d85c8a4b1d6df2373a29c43dd
kgxu <Kevinx88@gmail.com>
2019-05-29 14:10:30 -0400

    update styling

--- client/src/components/Hours.jsx
+++ client/src/components/Hours.jsx
@@ -9,17 +9,24 @@ class Hours extends React.Component {
     var weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
     return (
       <div id='hours_container'>
-        <div>
-          <h3 id='hours_h3'>Hours</h3>
-          <ul id='hours'>
-            {weekDays.map((day) => {
-              return (
-                <li><span class='day'>{day}</span><span class='timeToday'>{this.props.todayTime}</span></li>
-              )
-            })}
-          </ul>
+        <h3 id='hours_h3'>Hours</h3>
+        <div id='days_container'>
+        <ul id='days' class='days_list'>
+          {weekDays.map((day) => {
+            return (
+              <li><span class='day'>{day}</span></li>
+            )
+          })}
+        </ul>
+        <ul id='hours' class='days_list'>
+          {weekDays.map((day) => {
+            return (
+              <li><span class='timeToday'>{this.props.todayTime}</span></li>
+            )
+          })}
+        </ul>
         </div>
-        <a href='' id='edit_business'>Edit business info</a>
+        {/* <a href='' id='edit_business'>Edit business info</a> */}
       </div>
     )
   }

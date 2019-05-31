0a0cc164e317fbea2d29822e0bcce9e884d91e72
kgxu <Kevinx88@gmail.com>
2019-05-31 15:27:59 -0400

    adding cors middleware

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var app = express();
 var { requestOrderTableDataAction, requestRestaurantInfoDataAction } = require('./controller/index.js');
 var router = express.Router();
 var port = process.env.PORT || 3010;
 
+app.use(cors());
 app.use(express.static(path.join(__dirname + '/../client/dist')));
 app.use(express.static(path.join(__dirname + '/../public')));
 app.use(router);
